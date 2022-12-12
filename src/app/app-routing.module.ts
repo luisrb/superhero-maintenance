f7b01dd15572691e79a124306d094d1b61ae3b05
luisrb <luisos123@gmail.com>
2022-12-12 12:25:15 +0100

    Add Hero module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path: 'heroes',
+    loadChildren: () => import('./modules/heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'heroes'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
