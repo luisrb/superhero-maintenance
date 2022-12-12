6ee2ff6394bd47cb3f832cc71befa61354957aab
luisrb <luisos123@gmail.com>
2022-12-12 14:36:19 +0100

    Add heroes router

--- src/app/modules/heroes/heroes-routing.module.ts
+++ src/app/modules/heroes/heroes-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './ui/pages/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
