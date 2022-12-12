c43893578ea2acfcb1c5b26fedf82199254204f1
luisrb <luisos123@gmail.com>
2022-12-12 14:33:24 +0100

    Add heroes component

--- src/app/modules/heroes/heroes.module.ts
+++ src/app/modules/heroes/heroes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroesComponent } from './ui/pages/heroes.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroesComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
