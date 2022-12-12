735a30e5c7048645bf526ce5a357d8f157df3bd9
luisrb <luisos123@gmail.com>
2022-12-12 13:20:33 +0100

    Add enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8000/' // Needs to be changed to the actual API URL
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/', // Needs to be changed to the actual API URL
+};
